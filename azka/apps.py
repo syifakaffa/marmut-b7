1f60fd8d79d14b30a4507dcca671a9dd0d68d3e5
syifakaffa <syifa.kaffa@ui.ac.id>
2024-04-26 10:10:35 +0700

    init app

---
+++ azka/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AzkaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'azka'
---
+++ salma/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SalmaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'salma'
